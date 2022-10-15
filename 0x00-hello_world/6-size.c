2e2f799e07617fa8a43caa429be1f59c0d59f5e9
Tolulope77 <olubiyitolulope@gmail.com>
2022-10-15 15:11:03 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - is the entry point for the program.
- * return: zero if no error, non-zero values if errors.
+ * Return: zero if no error, non-zero values if errors.
  */
 int main(void)
 {
