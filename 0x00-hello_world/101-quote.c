10a889a42d4d25f20670e96494aaa487d911e7f4
Tolulope77 <olubiyitolulope@gmail.com>
2022-10-16 14:36:12 -0700

    101-quote.c updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - is the entry point of the program
+ * main - a C program that prints exactly and that piece of art is useful" 
+ * - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
  * Return: one on success.
  */
 int main(void)
