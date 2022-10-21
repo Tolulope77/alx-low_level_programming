30d05de82e86e2010414bfa64ce24e4c002a09d2
Tolulope Olubiyi <olubiyitolulope@gmail.com>
2022-10-21 04:52:38 +0000

    complete

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,9 +7,9 @@
 
 int main(void)
 {
-       	int i;
-
-       	printf("Infinite loop incoming :(\n");
+	int i;
+	
+	printf("Infinite loop incoming :(\n");
 	
 	i = 0;
 	
@@ -17,7 +17,6 @@ int main(void)
 	/*{*/
 	/*putchar(i);*/
 	/*}*/
-
 	printf("Infinite loop avoided! \\o/\n");
 	
 	return (0);
