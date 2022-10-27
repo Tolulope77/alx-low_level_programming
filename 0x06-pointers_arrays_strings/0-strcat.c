8553f424971af3deab1f0ffc6f6fb4a71c35dae0
Tolulope77 <olubiyitolulope@gmail.com>
2022-10-26 18:57:15 -0700

    more pointers arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int d_len = 0, i = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[i++])
-	{
-		d_len++;
-	}
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[d_len++] = src[i];
-	}
-	dest[d_len++] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 
 }
