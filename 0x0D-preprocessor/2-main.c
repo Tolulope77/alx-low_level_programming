c614d7f87f11a610a447f6f865b35894a9ec0e3b
Tolulope77 <olubiyitolulope@gmail.com>
2022-11-14 08:43:15 -0800

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * File: 2-main.c
+ * main - Prints the name of the file of the program
+ * was compiled from, followed by a new line.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
